9e95a118f6b0a542d927d655493d7f813793910c
nanorisrael <israel.nanor@compozelabs.com>
2023-07-21 07:06:33 +0000

    Add setup for quiz client

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    parser: 'typescript',
+    semi: true,
+    singleQuote: true,
+    // Other configuration options...
+    // plugins: [require('prettier-plugin-tailwindcss')]
+
+  };
+  
